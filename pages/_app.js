f4d989efe3ae2ab2688f81a658e6b9653ac7a33b
Anurag <100985214+anuragcar@users.noreply.github.com>
2025-02-22 14:49:27 -0500

    add vercel analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,16 +1,18 @@
-import { ChakraProvider } from '@chakra-ui/react'
-import Layout from '../components/layouts/main'
-import theme from '../components/theme'
-import '../styles/carousel.css'
+import { ChakraProvider } from "@chakra-ui/react";
+import { Analytics } from "@vercel/analytics/react";
+import Layout from "../components/layouts/main";
+import theme from "../components/theme";
+import "../styles/carousel.css";
 
 const Website = ({ Component, pageProps, router }) => {
-    return (
-        <ChakraProvider theme={theme}>
-            <Layout router={router}>
-                <Component {...pageProps} key={router.route} />
-            </Layout>
-        </ChakraProvider>
-    )
-}
+  return (
+    <ChakraProvider theme={theme}>
+      <Layout router={router}>
+        <Component {...pageProps} key={router.route} />
+      </Layout>
+      <Analytics /> 
+    </ChakraProvider>
+  );
+};
 
-export default Website
+export default Website;
